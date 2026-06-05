6e525b1e1582ac63ef9c99b3d636bab365eff24e
Kushal Gupta <144117891+Kushalgupta02@users.noreply.github.com>
2026-06-05 22:30:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (77.70%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -5,22 +5,32 @@ class Solution {
         }
         int m=matrix.length;
         int n=matrix[0].length;
-        int l=0;
-        int r=m*n;
-        while(l<r){
-            int mid=(l+r)/2;
-            int i=mid/n;
-            int j=mid%n;
-            if (matrix[i][j] == target){
-                return true;
-            }
-            if (matrix[i][j] < target){
-                l = mid + 1;
-            }
-            else{
-                r = mid;
+        int k=0;
+        int []mat=new int[m*n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                mat[k++]=matrix[i][j];
             }
         }
+        int low = 0, high = mat.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            // Check if x is present at mid
+            if (mat[mid] == target)
+                return true;
+
+            
+            if (mat[mid] < target)
+                low = mid + 1;
+
+            
+            else
+                high = mid - 1;
+        }
+
+        // If we reach here, then element was
+        // not present
         return false;
     }
 }
